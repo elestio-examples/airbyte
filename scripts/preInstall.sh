d1586a127d9cc1a5ab61cb9739934fb8dc4db005
jbenguira <z51biz@gmail.com>
2022-09-08 09:07:27 +0300

    fixed preinstall

--- scripts/preInstall.sh
+++ scripts/preInstall.sh
@@ -1,1 +1,13 @@
-chown -R 1001:1001 .
+mkdir -p ./db;
+mkdir -p ./tmp;
+mkdir -p ./workspace;
+mkdir -p ./data;
+mkdir -p ./local;
+mkdir -p ./temporal;
+
+chown -R 1001:1001 ./db;
+chown -R 1001:1001 ./tmp;
+chown -R 1001:1001 ./workspace;
+chown -R 1001:1001 ./data;
+chown -R 1001:1001 ./local;
+chown -R 1001:1001 ./temporal;
