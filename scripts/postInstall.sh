3a9a5ac41e3df368651b84434af113f2678d0dad
jbenguira <z51biz@gmail.com>
2022-09-10 22:31:05 +0300

    fixed scripts

--- scripts/postInstall.sh
+++
@@ -1,9 +0,0 @@
-#set correct local/docker root to access local files
-
-#remove default env vars for that
-sed -i '/LOCAL_ROOT=/d' .env
-sed -i '/LOCAL_DOCKER_MOUNT=/d' .env
-
-#add new env vars with the correct value
-echo "LOCAL_ROOT=${PWD}/local" >> .env
-echo "LOCAL_DOCKER_MOUNT=${PWD}/local" >> .env
--- scripts/preInstall.sh
+++ scripts/preInstall.sh
@@ -11,3 +11,15 @@ chown -R 1001:1001 ./workspace;
 chown -R 1001:1001 ./data;
 chown -R 1001:1001 ./local;
 chown -R 1001:1001 ./temporal;
+
+
+#set correct local/docker root to access local files
+
+#remove default env vars for that
+sed -i '/LOCAL_ROOT=/d' .env
+sed -i '/LOCAL_DOCKER_MOUNT=/d' .env
+
+#add new env vars with the correct value
+echo "LOCAL_ROOT=${PWD}/local" >> .env
+echo "LOCAL_DOCKER_MOUNT=${PWD}/local" >> .env
+
