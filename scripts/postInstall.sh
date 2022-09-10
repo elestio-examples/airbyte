e20ce4ecf549086c0a7941441b52cf053866d7f5
jbenguira <z51biz@gmail.com>
2022-09-10 22:24:37 +0300

    fixed local path

--- scripts/postInstall.sh
+++ scripts/postInstall.sh
@@ -5,5 +5,5 @@ sed -i '/LOCAL_ROOT=/d' .env
 sed -i '/LOCAL_DOCKER_MOUNT=/d' .env
 
 #add new env vars with the correct value
-echo "LOCAL_ROOT=${PWD}local" >> .env
-echo "LOCAL_DOCKER_MOUNT=${PWD}local" >> .env
+echo "LOCAL_ROOT=${PWD}/local" >> .env
+echo "LOCAL_DOCKER_MOUNT=${PWD}/local" >> .env
